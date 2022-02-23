1a18bd444e0a4a5e2d0a9ee7f8a64e2d1cbad01f
Danilo Trotta <sharaku17@gmail.com>
2022-02-24 00:03:47 +0100

    fix strokeWidth

--- components/ParaphraseSection.js
+++ components/ParaphraseSection.js
@@ -757,7 +757,7 @@ const ParaphraseSection = () => {
                       cy="12"
                       r="10"
                       stroke="currentColor"
-                      stroke-width="4"
+                      strokeWidth="4"
                     ></circle>
                     <path
                       className="opacity-75"
